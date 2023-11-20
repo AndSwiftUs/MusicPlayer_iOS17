d65a2671fbd91fd44653e38d953c594b1749d612
Andrew Us <AndSwiftUs@gmail.com>
2023-11-20 12:47:53 +0300

    Initial Commit

---
+++ mp17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mp17
+//
+//  Created by Andrew Us on 20.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mp17/mp17App.swift
@@ -0,0 +1,17 @@
+//
+//  mp17App.swift
+//  mp17
+//
+//  Created by Andrew Us on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct mp17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
